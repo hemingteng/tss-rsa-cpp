d72c34c2bed3a9fa9dbaf412d73097ccc6de0de1
何剑虹 <hejh1500@gmail.com>
2022-06-11 10:59:10 +0800

    Update example

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,6 +1,7 @@
 #include "crypto-bn/bn.h"
 #include "exception/safeheron_exceptions.h"
 #include "crypto-tss-rsa/tss_rsa.h"
+#include "crypto-encode/hex.h"
 
 using safeheron::bignum::BN;
 using safeheron::tss_rsa::RSAPrivateKeyShare;
@@ -36,25 +37,31 @@ int main(int argc, char **argv) {
     priv_arr[2].ToJsonString(json_str);
     std::cout << "private key share 3: "  << json_str << std::endl;
 
+    // Prepare
+    std::string doc_pss = safeheron::tss_rsa::EncodeEMSA_PSS(doc, key_bits_length, safeheron::tss_rsa::SaltLength::AutoLength);
+    std::cout << "EM: " << safeheron::encode::hex::EncodeToHex(doc) << std::endl;
     // Party 1 sign.
-    RSASigShare sig_share0 = priv_arr[0].Sign(doc, key_meta, pub);
+    RSASigShare sig_share0 = priv_arr[0].Sign(doc_pss, key_meta, pub);
     sig_share0.ToJsonString(json_str);
     std::cout << "signature share 1: " << json_str << std::endl;
     // Party 3 sign.
-    RSASigShare sig_share2 = priv_arr[2].Sign(doc, key_meta, pub);
+    RSASigShare sig_share2 = priv_arr[2].Sign(doc_pss, key_meta, pub);
     sig_share2.ToJsonString(json_str);
     std::cout << "signature share 3: " <<  json_str << std::endl;
 
     // Combine signatures
     // Distributed signature
     std::vector<RSASigShare> sig_share_arr;
-    sig_share_arr.push_back(sig_share0);
-    sig_share_arr.push_back(sig_share2);
+    for(int i = 0; i < l; i++) {
+        sig_share_arr.emplace_back(priv_arr[i].Sign(doc_pss, key_meta, pub));
+    }
     BN sig;
-    status = safeheron::tss_rsa::CombineSignatures(doc, sig_share_arr, pub, key_meta, sig);
-    std::cout << "final signature = 0x" << sig.Inspect() << std::endl;
+    bool ok = safeheron::tss_rsa::CombineSignatures(doc_pss, sig_share_arr, pub, key_meta, sig);
+    std::cout << "succeed to sign: " << ok <<std::endl;
+    std::cout << "signature: " << sig.Inspect() <<std::endl;
 
     // Verify the final signature.
-    std::cout<< "verify:" << pub.VerifySignature(doc, sig) << std::endl;
+    std::cout << "Verify Pss: " << safeheron::tss_rsa::VerifyEMSA_PSS(doc, key_bits_length, safeheron::tss_rsa::SaltLength::AutoLength, doc_pss) << std::endl;
+    std::cout << "Verify Sig: " << pub.VerifySignature(doc_pss, sig) << std::endl;
     return 0;
 }
